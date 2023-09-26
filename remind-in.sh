222a64100e7daba0e9fc8a0c293bf0ea39ebfef6
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2023-09-26 23:46:42 +0200

    add remind-jobs-done func

--- remind-in.sh
+++ remind-in.sh
@@ -2,6 +2,19 @@
 #
 # Bash functions to Remind in ## seconds
 
+# Properties that can be set for each task
+#   name (text) : the name of the reminder
+#   id (text, r/o) : the unique identifier of the reminder
+#   body (text) : the notes attached to the reminder
+#   completed (boolean) : Is the reminder completed?
+#   completion date (date) : the completion date of the reminder
+#   container (list, r/o) : the container of the reminder
+#   creation date (date, r/o) : the creation date of the reminder
+#   due date (date) : the due date of the reminder
+#   modification date (date, r/o) : the modification date of the reminder
+#   remind me date (date) : the remind date of the reminder
+#   priority (integer) : the priority of the reminder
+
 remind-in() {
     body=$(cat)
     delay=$1
@@ -20,4 +33,30 @@ remind-in() {
          end tell
     end tell
 "
+}
+
+remind-job-done() {
+    if [[ "$*" == "" ]]
+    then
+        args=%%
+    else
+        args=$*
+    fi
+
+    for j in $args
+    do
+        info=$(jobs $j)
+        if [[ "$info" != "" ]]
+        then
+            pid=$(jobs -p $j | sed 's/\[[0-9]\]* [ -+]* \([0-9]*\) .*/\1/')
+            lstat=$(ps -o lstart= -p $pid)
+            descr="Running since $lstat\nWaited since $(date)"
+            echo -e "$descr" | if fg $j
+            then
+                remind-in 0 "👌 $info"
+            else
+                remind-in 0 "🤌 $info"
+            fi
+        fi
+    done
 }
